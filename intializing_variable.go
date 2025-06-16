ce12cab6c046405858eb1a7841df5c0e784e4089
Samrat Neupane <samrat.neupane17013@gmail.com>
2025-06-16 13:03:21 +0545

    learning go intitializing vairables and shorthanddecl

---
+++ intializing_variable.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	// var a int
+	// var b string
+
+	// a =3
+	// b="wow"
+
+	// var a = 3
+	// var b = "wow"
+
+	var i int
+	var fl float32
+	var b bool
+	var str string
+	var arr [3] int
+	 var slice [] int
+	 var m map[string]int 
+	fmt.Println(i,fl,b, str, arr, slice, m)
+}
---
+++ shorthanddeclaration.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	//no need to use var keyword
+
+	a := 3
+
+	fmt.Printf("a - %d\n", a)
+
+	arr := [3] int {1,2,3}
+
+	fmt.Printf("arr = %v\n", arr)
+}
